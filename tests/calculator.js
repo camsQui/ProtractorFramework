e8fae381c46c6f6543545316559f0e5db65c0206
camsQui <c.quimera@arcanys.com>
2023-09-06 19:38:10 +0800

    subtraction test removed

--- tests/calculator.js
+++ tests/calculator.js
@@ -25,19 +25,19 @@ describe('demo calculator tests', function(){
     });
 
 
-    it('subtraction test', function(){
+    // it('subtraction test', function(){
 
-        browser.get('https://juliemr.github.io/protractor-demo/');
-        element(by.model('first')).sendKeys('4');
-        element(by.model('second')).sendKeys('16');
-        element(by.css('[ng-click="doAddition()"]555')).click();
+    //     browser.get('https://juliemr.github.io/protractor-demo/');
+    //     element(by.model('first')).sendKeys('4');
+    //     element(by.model('second')).sendKeys('16');
+    //     element(by.css('[ng-click="doAddition()"]555')).click();
 
-        let result = element(by.cssContainingText('.ng-binding', '20'));
-        expect(result.getText()).toEqual('20');
+    //     let result = element(by.cssContainingText('.ng-binding', '20'));
+    //     expect(result.getText()).toEqual('20');
 
 
-        browser.sleep(3000);        
-    });
+    //     browser.sleep(3000);        
+    // });
 
 });
 
